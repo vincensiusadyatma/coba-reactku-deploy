6968e903f70a74586ece890b9ab910ef367155d3
vincensiusadyatma <adyatmav@gmail.com>
2024-01-04 14:04:23 +0700

    commit kedua

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Mengganti BrowserRouter dengan HashRouter
+import './index.css';
 
-import './index.css'
-
-import FormLogin from "./pages/FormLogin"
-import FormDaftar from "./pages/FormDaftar"
-import ProductPage from "./pages/ProductPage"
-
-import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FormLogin from './pages/FormLogin';
+import FormDaftar from './pages/FormDaftar';
+import ProductPage from './pages/ProductPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/"element={<FormLogin/>}></Route>
-        <Route path="/daftar"element={<FormDaftar/>}></Route>
-        <Route path="/products"element={<ProductPage/>}></Route>
+        <Route path="/" element={<FormLogin />} />
+        <Route path="/daftar" element={<FormDaftar />} />
+        <Route path="/products" element={<ProductPage />} />
       </Routes>
     </Router>
   </React.StrictMode>,
 );
-
