674473e07ab3737777bb74f57dd6e83ca7b0e69d
vincensiusadyatma <adyatmav@gmail.com>
2024-01-04 16:01:14 +0700

    perbaikan lagi

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Switch, Route } from 'react-router-dom'; // Mengganti BrowserRouter dengan HashRouter
+import { HashRouter, Routes, Route } from 'react-router-dom'; // Mengganti BrowserRouter dengan HashRouter
 import './index.css';
 
 import FormLogin from './pages/FormLogin';
@@ -10,11 +10,11 @@ import ProductPage from './pages/ProductPage';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <Switch>
+      <Routes>
         <Route path="/" element={<FormLogin />} />
         <Route path="/daftar" element={<FormDaftar />} />
         <Route path="/products" element={<ProductPage />} />
-      </Switch>
+      </Routes>
     </HashRouter>
   </React.StrictMode>,
 );
