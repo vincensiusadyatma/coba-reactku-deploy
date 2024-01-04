a1fe878efee550c3eab516f9bce28a5a4dee4548
vincensiusadyatma <adyatmav@gmail.com>
2024-01-04 16:08:46 +0700

    menambahkan error 404

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './index.css';
 import FormLogin from './pages/FormLogin';
 import FormDaftar from './pages/FormDaftar';
 import ProductPage from './pages/ProductPage';
+import error404 from './pages/404';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<FormLogin />} />
         <Route path="/daftar" element={<FormDaftar />} />
         <Route path="/products" element={<ProductPage />} />
+        <Route exact path="*" element={error404}></Route>
       </Routes>
     </HashRouter>
   </React.StrictMode>,
